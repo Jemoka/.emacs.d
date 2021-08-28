95bb538f598baf3492d84d906586431d8c339757
Houjun Liu <hliu@shabang.cf>
2021-08-28 07:33:24 -0700

    Working? Config... I guess?
    Man this is scary.

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit flycheck pyvenv company-jedi evil-easymotion company company-mode powerline doom-themes evil-nerd-commenter evil-leader evil-surround undo-tree evil-collection evil use-package))
+ '(warning-suppress-log-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,213 @@
+;; So. so. so. Let's try this again. Will jack succeed this time?
+;; Probably not. But it's worth a try.
+
+
+
+;; ----Shunning custom
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+
+
+;; ----Litter in one place
+(setq backup-directory-alist `(("." . "~/.saves")))
+(setq create-lockfiles nil)
+
+
+
+;; ----Package System
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+
+
+;; ----use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+
+
+;; ----evil
+;; OG Evil
+(use-package evil
+  :init
+  (setq evil-want-keybinding nil)
+  :config
+  ;; Evil mode
+  (evil-mode 1)
+
+  ;; HJKL Navigation
+  (define-key evil-normal-state-map (kbd "C-h") #'evil-window-left)
+  (define-key evil-normal-state-map (kbd "C-j") #'evil-window-down)
+  (define-key evil-normal-state-map (kbd "C-k") #'evil-window-up)
+  (define-key evil-normal-state-map (kbd "C-l") #'evil-window-right))
+
+;; More Evil
+(use-package evil-collection
+  :config
+  (evil-collection-init))
+
+;; Undo
+(use-package undo-tree
+  :after evil
+  :config
+  (global-undo-tree-mode)
+  (evil-set-undo-system 'undo-tree))
+
+;; Tim Pope's Surround but like Evil
+(use-package evil-surround
+  :config
+  (global-evil-surround-mode 1))
+
+;; Evil Leader
+(use-package evil-leader
+  :config
+  (evil-leader/set-leader "<SPC>")
+  (global-evil-leader-mode))
+
+;; Nerd Commentation
+(use-package evil-nerd-commenter
+  :config
+  (evil-leader/set-key
+    "cc" 'evilnc-comment-or-uncomment-lines
+    "cu" 'evilnc-comment-or-uncomment-lines))
+
+;; Easy motion
+(use-package evil-easymotion
+  :config
+  (evilem-default-keybindings "SPC"))
+
+
+
+;; ----cosmetics
+;; Bars, menus, edges... Begone!
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark)) 
+(setq frame-title-format '("\n emacs"))
+
+;; Doom theme
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t 
+	doom-themes-enable-italic t)
+  (load-theme 'doom-opera t)
+
+  (doom-themes-visual-bell-config)
+  (doom-themes-neotree-config)
+  (setq doom-themes-treemacs-theme "doom-atom")
+  (doom-themes-treemacs-config)
+  (doom-themes-org-config))
+
+;; Powerline! POWER!
+(use-package powerline
+  :config
+  (powerline-default-theme))
+
+;; Line numbers, relativity
+(add-hook 'prog-mode-hook (lambda ()
+			    (display-line-numbers-mode)
+			    (setq display-line-numbers-type 'relative)))
+
+;; Scroll line by line
+(setq scroll-step            1
+      scroll-conservatively  10000)
+
+
+
+;; ----autocomplete
+
+;; Turns out the best way to do LSP is to not use it
+(use-package company
+  :init
+  (setq company-minimum-prefix-length 1)
+  (setq company-idle-delay 0)
+  (setq company-format-margin-function nil)
+  
+  :config
+  (define-key company-active-map (kbd "TAB") 'company-select-next)
+  (define-key company-active-map (kbd "<backtab>") 'company-select-previous)
+  (define-key company-active-map (kbd "RET") nil)
+  (global-company-mode)
+  :hook
+  (after-init . company-tng-mode))
+
+;; Python
+(use-package company-jedi
+  :config ;; we set company backends here so as to have jedi
+  (setq company-backends '((company-files company-jedi
+			    company-capf company-clang
+			    company-keywords company-dabbrev-code
+			    company-etags company-dabbrev company-semantic))))
+
+;; Python venvs
+(use-package pyvenv)
+
+;; Flycheck
+(use-package flycheck
+  :init
+  (global-flycheck-mode)
+  :config
+  (setq flycheck-check-syntax-automatically '(mode-enabled save))
+  (set-face-attribute 'flycheck-error nil :underline t)
+  (set-face-attribute 'flycheck-warning nil :underline t)
+  (set-face-attribute 'flycheck-info nil :underline t)
+  (define-fringe-bitmap 'my-flycheck-fringe-indicator
+    (vector #b00000000
+            #b00000000
+            #b00000000
+            #b00000000
+            #b00000000
+            #b00000000
+            #b00000000
+            #b00011100
+            #b00111110
+            #b00111110
+            #b00111110
+            #b00011100
+            #b00000000
+            #b00000000
+            #b00000000
+            #b00000000
+            #b00000000))
+  (let ((bitmap 'my-flycheck-fringe-indicator))
+    (flycheck-define-error-level 'error
+      :severity 2
+      :overlay-category 'flycheck-error-overlay
+      :fringe-bitmap bitmap
+      :error-list-face 'flycheck-error-list-error
+      :fringe-face 'flycheck-fringe-error)
+    (flycheck-define-error-level 'warning
+      :severity 1
+      :overlay-category 'flycheck-warning-overlay
+      :fringe-bitmap bitmap
+      :error-list-face 'flycheck-error-list-warning
+      :fringe-face 'flycheck-fringe-warning)
+    (flycheck-define-error-level 'info
+      :severity 0
+      :overlay-category 'flycheck-info-overlay
+      :fringe-bitmap bitmap
+      :error-list-face 'flycheck-error-list-info
+      :fringe-face 'flycheck-fringe-info)))
+
+
+
+;; ----developer tools
+(use-package magit)
+
+
+
+;; ----misc
+(setq c-basic-offset 4)
+
+(provide 'init)
+;;; init.el ends here
